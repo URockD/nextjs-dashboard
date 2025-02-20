c09d7e6382cdc7c94b5589c998d32e6ec4a1fd74
Rodrigo Gonzalez <vampiredd@live.com>
2025-02-19 20:42:49 -0600

    Removed customers module

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -15,7 +15,7 @@ const links = [
   {
     name: 'Invoices', href: '/dashboard/invoices', icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  //{ name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
